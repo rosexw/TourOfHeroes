191f6ee30b941f19d8a586c395b4b78ee349ed68
rosexw <rose.xw.wan@gmail.com>
2019-02-10 19:26:24 -0500

    https://angular.io/tutorial/toh-pt1 Added components and templates to show hero details in uppercase

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
