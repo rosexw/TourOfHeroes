d9883f96ce408b2b4d08049353d62efc5e9a8ef2
rosexw <rose.xw.wan@gmail.com>
2019-02-10 20:05:31 -0500

    Completed step 1 - the hero editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+ 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
